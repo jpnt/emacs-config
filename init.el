e9a3903a00a125a375052969f9a1fd79de5c0735
Joao Pinto <joaopintojpgp@gmail.com>
2022-12-13 16:28:13 +0000

    update init.el

--- init.el
+++ init.el
@@ -1,10 +1,14 @@
-(load-theme 'deeper-blue)
+(load-theme 'modus-vivendi)
 
 (prefer-coding-system 'utf-8-unix)
 (set-language-environment "UTF-8")
+(global-display-line-numbers-mode t)
+(recentf-mode t)
+(save-place-mode t)
+
 (setq iso-transl-char-map nil)
 (setq inhibit-startup-screen t)
-(setq backup-directory-alist '(("." . "~/.emacs.d/.emacs_saves")))
+(setq auto-mode-case-fold nil)
 
 (defvar user-setup-directory          (expand-file-name "setUp"          user-emacs-directory))
 (defvar user-setup-builtins-directory (expand-file-name "setup/builtins" user-emacs-directory))
@@ -13,6 +17,7 @@
 (defvar user-cache-directory          (expand-file-name ".cache"         user-emacs-directory))
 (defvar user-bin-directory            (expand-file-name "bin"            "~"))
 (setq custom-file                     (expand-file-name "custom.el"      user-emacs-directory))
+(setq backup-directory-alist '(("." . "~/.emacs.d/.emacs_saves")))
 (make-directory user-cache-directory t)
 
 (require 'package)
@@ -28,20 +33,18 @@
         use-package-expand-minimally t))
 
 ;; Packages
+(use-package projectile)
 
-(use-package smex)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(use-package smex
+  :config
+  (global-set-key (kbd "M-x") 'smex)                      
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands))
 
 (use-package better-defaults)
 
-(use-package projectile)
-
 (use-package markdown-mode
-  :ensure t
-  :mode ("README\\.md\\'" . gfm-mode)
-  :init (setq markdown-command "multimarkdown"))
-
+  :mode ("\\.md\\'" . markdown-mode))
+  
 (use-package magit
   :bind (("C-x g" . magit-status)
          ("C-x C-g" . magit-status)))
@@ -52,12 +55,6 @@
 
 (use-package evil)
 
-(use-package flex-autopair)
-(use-package flex-compile)
-(use-package flex-isearch)
-
 (use-package rainbow-delimiters)
 
 (use-package company)
-
-(use-package undo-tree)
